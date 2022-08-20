21960c20d83df28d19829f0cbd9aac0fc27a3e32
Patch-o <francisco.javier.climent@bootcamp-upgrade.com>
2022-08-20 19:54:22 +0200

    primer push/pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CharactersComponent } from './pages/characters/characters.component';
+import { ChronologyComponent } from './pages/chronology/chronology.component';
+import { HousesComponent } from './pages/houses/houses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CharactersComponent,
+    ChronologyComponent,
+    HousesComponent
   ],
   imports: [
     BrowserModule,
