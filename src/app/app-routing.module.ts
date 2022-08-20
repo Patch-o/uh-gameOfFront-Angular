fe3954049e6e88f877621622be6430ddc5356a34
Patch-o <francisco.javier.climent@bootcamp-upgrade.com>
2022-08-20 20:39:37 +0200

    tercer push/navbar y rutas1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { ChronologyComponent } from './pages/chronology/chronology.component';
+import { HousesComponent } from './pages/houses/houses.component';
+import { CharactersComponent } from './pages/characters/characters.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "characters", component: CharactersComponent },
+  {path: "houses", component: HousesComponent},
+  {path: "chronology", component: ChronologyComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
 import { CharactersComponent } from './pages/characters/characters.component';
 import { ChronologyComponent } from './pages/chronology/chronology.component';
-import { HousesComponent } from './pages/houses/houses.component';
-import { GalleryCharactersComponent } from './shared/components/gallery-characters/gallery-characters.component';
-import { GalleryHousesComponent } from './shared/components/gallery-houses/gallery-houses.component';
-import { GalleryChronologyComponent } from './shared/components/gallery-chronology/gallery-chronology.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
+    
     CharactersComponent,
-    ChronologyComponent,
-    HousesComponent,
-    GalleryCharactersComponent,
-    GalleryHousesComponent,
-    GalleryChronologyComponent
+    ChronologyComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
