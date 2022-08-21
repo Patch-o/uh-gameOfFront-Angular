65fd644efa00a7f276588b511a5af6e984667f30
Patch-o <francisco.javier.climent@bootcamp-upgrade.com>
2022-08-21 17:06:42 +0200

    sexto push/ CSS houses y characters, libreria scroll y translate instalada, ultima version de npm instalada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { HousesComponent } from './pages/houses/houses.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+// import {TranslateModule} from '@ngx-translate/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CharactersComponent } from './pages/characters/characters.component';
@@ -20,11 +20,13 @@ import { GalleryChronologyComponent } from './shared/components/gallery-chronolo
     CharactersComponent,
     ChronologyComponent,
     HousesComponent,
+
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // TranslateModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/components/gallery-characters/gallery-characters.component.ts
+++ src/app/shared/components/gallery-characters/gallery-characters.component.ts
@@ -8,7 +8,8 @@ import { Component, Input, OnInit } from '@angular/core';
 export class GalleryCharactersComponent implements OnInit {
  
   @Input() public character : any = [];
-
+  @Input() truffle: any = {src:'https://i.gifer.com/origin/29/29c00d14bf13ea159e7c2b735a5839bc.gif',
+  name:'gordito'}
   constructor() { }
 
   ngOnInit(): void {
