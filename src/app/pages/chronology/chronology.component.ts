df89e082d1245666836363cefbda0da43bdfeb9c
Patch-o <francisco.javier.climent@bootcamp-upgrade.com>
2022-08-28 09:17:12 +0200

    responsive characters & foot-nav & home, gallery css

--- src/app/pages/chronology/chronology.component.ts
+++ src/app/pages/chronology/chronology.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ServiceChronologyService } from 'src/app/shared/services/service-chronology.service';
 
 @Component({
   selector: 'app-chronology',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChronologyComponent implements OnInit {
 
-  constructor() { }
+  chronology: any={};
+  ageList: number[] = [];
+
+  constructor(
+    private pajarito: ServiceChronologyService
+  ) { }
 
   ngOnInit(): void {
+    this.pajarito.getChrono().subscribe((res:any) => {
+      this.chronology = res;
+            console.log(this.chronology);
+
+        let list =[];
+      for (let i = 0; i < this.chronology.length; i++) {
+        let chrono = this.chronology[i];
+         this.ageList= chrono.age.age       
+         
+         console.log(this.ageList);
+        }
+        // this.ageList= list;
+        // console.log(this.ageList);
+      
+      
+      
+      
+    })
   }
 
 }
